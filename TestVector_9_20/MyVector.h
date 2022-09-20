7fd4bcf384e1386674ef121cc9b27aab9f73ad5a
soukenki <1106671528@qq.com>
2022-09-20 18:46:15 +0900

    9/20 更新

---
+++ TestVector_9_20/MyVector.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+namespace bit
+{
+	class vector
+	{
+
+	public:
+
+
+	private:
+
+	};
+
+	void TestVector1()
+	{
+
+	}
+}
---
+++ TestVector_9_20/Test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include "MyVector.h"
+
+int mian()
+{
+	try
+	{
+		bit::TestVector1();
+	}
+	catch (const exception& e) // Å×Òì³£´¦Àí
+	{
+		cout << e.what() << endl;
+	}
+
+	return 0;
+}
