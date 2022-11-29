3a32ef94d4ac45e991c1a326de4bf26d10197aae
soukenki <1106671528@qq.com>
2022-11-30 02:05:15 +0900

    11/30 初步更新RBTree

---
+++ RedBlackTree/RedBlackTree.h
@@ -0,0 +1,81 @@
+#pragma once
+
+enum Colour
+{
+	RED,
+	BLACK
+};
+
+template<class K, class V>
+struct RBTreeNode
+{
+	RBTreeNode<K, V>* _left;
+	RBTreeNode<K, V>* _right;
+	RBTreeNode<K, V>* _parent;
+
+	pair<K, V> _kv;   // key->first   value->second
+	Colour _col;
+
+	RBTreeNode(const pair<K, V>& kv)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _kv(kv)
+	{}
+};
+
+template<class K, class V>
+struct RBTree
+{
+	typedef RBTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			_root->_col = BLACK;
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else
+			{
+				// 相等
+				return false;
+			}
+		}
+
+		// 到这里cur为空,创建+链接
+		cur = new Node(kv);
+		cur->_col = RED;
+
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		cur->_parent = parent;
+
+		if ()
+	}
+
+private:
+	Node* _root = nullptr;
+};
