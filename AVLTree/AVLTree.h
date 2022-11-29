7439e58824c33f07eb33ec115fbef07bb5ba0539
soukenki <1106671528@qq.com>
2022-11-30 00:41:03 +0900

    11/30 更新AVLTree

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -1,25 +1,33 @@
 #pragma once
 
 #include <assert.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+
 
 template<class K, class V>
 struct AVLTreeNode
 {
-	AVLTree(const pair<K, V>& kv)
-		:_left(nullptr)
-		,_right(nullptr)
-		,_parent(nullptr)
-		,_kv(kv)
-		,_bf(0)
-	{}
-
 	AVLTreeNode<K, V>* _left;
 	AVLTreeNode<K, V>* _right;
 	AVLTreeNode<K, V>* _parent;
 
-	pair<K, V> _kv;  // key->first   value->second
-	int _bf;   // balance factor  平衡因子 
-	// -1 0 1 分别代表 左中右
+	pair<K, V> _kv;    // key->first   value->second
+	int _bf;          // balance factor  平衡因子 
+					// -1 0 1 分别代表 左中右
+
+	AVLTreeNode(const pair<K, V>& kv)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _kv(kv)
+		, _bf(0)
+	{}
+
 };
 
 template<class K, class V>
@@ -105,6 +113,14 @@ public:
 				{
 					RotateLR(parent); // 左右双旋
 				}
+				else if (parent->_bf == 2 && cur->_bf == -1) // 右左
+				{
+					RotateRL(parent); // 右左双旋
+				}
+				else
+				{
+					assert(false);
+				}
 
 				break;
 			}
@@ -116,9 +132,47 @@ public:
 		return true;
 	}
 
+	void InOrder()
+	{
+		_InOrder(_root);
+		cout << endl;
+	}
 
-
+	bool IsBalance()
+	{
+		return _IsBalance(_root);
+	}
 private:
+	bool _IsBalance(Node* root) // 递归求树的高度差 判断树是否平衡
+	{
+		if (root == nullptr)
+		{
+			return true;
+		}
+
+		int leftHT = Height(root->_left);
+		int rightHT = Height(root->_right);
+		int diff = rightHT - leftHT;
+		
+		if (diff != root->_bf)
+		{
+			cout << root->_kv.first << "平衡因子异常" << endl;
+			return false;
+		}
+
+		return abs(diff) < 2
+			&& _IsBalance(root->_left)
+			&& _IsBalance(root->_right);  // abs绝对值  
+	}
+
+	int Height(Node* root)  // 树的高度
+	{
+		if (root == nullptr)
+			return 0;
+
+		return max(Height(root->_left), Height(root->_right)) + 1;
+	}
+
 	void RotateL(Node* parent)  // 左单旋
 	{
 		Node* subR = parent->_right;
@@ -135,7 +189,7 @@ private:
 		subR->_left = parent;
 		parent->_parent = subR;
 
-		if (_root == nullptr)
+		if (_root == parent)
 		{
 			_root = subR;
 			subR->_parent = nullptr;
@@ -224,6 +278,48 @@ private:
 		}
 	}
 
+	void RotateRL(Node* parent) // 右左双旋
+	{
+		Node* subR = parent->_right; 
+		Node* subRL = subR->_left; 
+		int bf = subRL->_bf;
+
+		RotateR(parent->_right);
+		RotateL(parent);
+
+		subRL->_bf = 0;
+		if (bf == 1)
+		{
+			subR->_bf = 0;
+			parent->_bf = -1;
+		}
+		else if (bf == -1)
+		{
+			subR->_bf = 1;
+			parent->_bf = 0;
+		}
+		else if (bf == 0)
+		{
+			parent->_bf = 0;
+			subR->_bf = 0;
+		}
+		else
+		{
+			assert(false);
+		}
+	}
+
+	void _InOrder(Node* root)   // 中序遍历
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+
+		_InOrder(root->_left);
+		cout << root->_kv.first << ":" << root->_kv.second << endl;
+		_InOrder(root->_right);
+	}
 
 private:
 	Node* _root = nullptr;
