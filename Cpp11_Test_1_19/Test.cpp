7819ea7f02354eb689110fb377da2776f8916f63
soukenki <1106671528@qq.com>
2023-01-27 03:14:37 +0900

    1/27 更新

--- Cpp11_Test_1_19/Test.cpp
+++ Cpp11_Test_1_19/Test.cpp
@@ -8,25 +8,26 @@
 #include <string>
 #include <array>
 #include <assert.h>
+#include <algorithm>
 
 using namespace std;
 
-class Date
-{
-public:
-	Date(int year, int month, int day)
-		:_year(year)
-		, _month(month)
-		, _day(day)
-	{
-		cout << "Date(int year, int month, int day)" << endl;
-	}
-
-private:
-	int _year;
-	int _month;
-	int _day;
-};
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		:_year(year)
+//		, _month(month)
+//		, _day(day)
+//	{
+//		cout << "Date(int year, int month, int day)" << endl;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
 
 //int main()
 //{
@@ -289,19 +290,413 @@ skk::string to_string(int value)
 //}
 
 
+//int main()
+//{
+//	//bit::string str1("hello");
+//	//bit::string str2(str1); //  拷贝构造
+//	//bit::string str3(move(str1)); // 移动构造
+//
+//
+//	std::string s1("hello world");
+//	std::string s2(s1); // 拷贝构造
+//
+//	// std::string s3(s1+s2);
+//	std::string s3 = s1 + s2; // 移动构造
+//	std::string s4 = move(s1);   // move()之后，s1的资源就可被转移，谨慎使用
+//
+//	return 0;
+//}
+
+
+//// 要求delete关键字实现，一个类，只能在堆上创建对象
+//class HeapOnly
+//{
+//public:
+//	HeapOnly()
+//	{
+//		_str = new char[10];
+//	}
+//
+//	~HeapOnly() = delete;
+//
+//	void Destroy()
+//	{
+//		delete[] _str;
+//
+//		operator delete(this);
+//	}
+//
+//private:
+//	char* _str;
+//	// ..
+//};
+//
+//int main()
+//{
+//	// HeapOnly hp1;  // 栈  err
+// 	// static HeapOnly hp2;  // 静态区  err
+//
+//	HeapOnly* ptr = new HeapOnly;  // 堆  因为在
+//
+//	ptr->Destroy();
+//	// operator delete(ptr); // OK
+//	return 0;
+//}
+//
+
+
+
+//// 可变参数的函数模板
+//template<class ...Args>
+//void ShowList(Args... args)
+//{
+//	cout << sizeof...(args) << endl;
+//
+//	// 不支持下面这样写，去读数据包中的数据 err
+//	//for (int i = 0; i < sizeof...(args); ++i)
+//	//{
+//	//	cout << args[i] << " ";
+//	//}
+//	//cout << endl;
+//}
+//
+//int main()
+//{
+//	string str("hello");
+//	ShowList();
+//	ShowList(1);
+//	ShowList(1, 'A');
+//	ShowList(1, 'A', str);
+//
+//	return 0;
+//}
+
+
+//void ShowList()  // 递归到零个参数时，匹配这个函数，相当于结束递归
+//{
+//	cout << endl;
+//}
+//
+//// Args... args  代表N个参数包 （N >= 0）
+//template<class T, class ...Args>
+//void ShowList(const T& val, Args... args)
+//{
+//	cout << "ShowList(" << val << ", " << sizeof...(args) << "参数包)" << endl;
+//
+//	ShowList(args...);  // 每次递归的传参都会-1， 编译时递归，不是运行时递归
+//}
+//
+//int main()
+//{
+//	string str("hello");
+//	ShowList(1, 'A', str);
+//	ShowList(1, 'A', str, 5.555, "asdasf");
+//
+//	return 0;
+//}
+
+
+
+void ShowList()  // 递归到零个参数时，匹配这个函数，相当于结束递归
+{
+	cout << endl;
+}
+
+template<class T>
+int PrintArg(const T& x)
+{
+	cout << x << " ";
+	return 0;
+}
+
+template<class ...Args>
+void ShowList(Args... args)
+{
+	//int a[] = { (PrintArg(args), 0)... }; // 逗号表达式，初始化出来的实际值为0 ，函数返回值void
+	int a[] = { PrintArg(args)... };  // 利用每次传参进行打印，函数返回值0
+	cout << endl;
+}
+
+//int main()
+//{
+//	string str("hello");
+//	ShowList(1, 'A', str);
+//	ShowList(1, 'A', str, 5.555, "asdasf");
+//
+//	return 0;
+//}
+
+
+
+class Date
+{
+public:
+	Date(int year = 1, int month = 1, int day = 1)
+		:_year(year)
+		, _month(month)
+		, _day(day)
+	{
+		cout << "Date(int year = 1, int month = 1, int day = 1)" << endl;
+	}
+
+	Date(const Date& d)
+		:_year(d._year)
+		, _month(d._month)
+		, _day(d._day)
+	{
+		cout << "Date(const Date& d)" << endl;
+	}
+
+	Date& operator=(const Date& d)
+	{
+		cout << "Date& operator=(const Date& d))" << endl;
+		return *this;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+//int main()
+//{
+//	// 没有区别
+//	vector<int> v1;
+//	v1.push_back(1);
+//	v1.emplace_back(2);
+//
+//	/*vector<pair<bit::string, int>> v2;
+//	v2.push_back(make_pair("sort", 1));
+//
+//	v2.emplace_back(make_pair("sort", 1));
+//	v2.emplace_back("sort", 1);*/
+//
+//	/*vector<Date> v3;
+//	v3.push_back(Date(2022,11,16));
+//
+//	cout <<"---------------------------------"<<endl;
+//	v3.emplace_back(2022, 11, 16);*/
+//
+//	list<Date> lt1;
+//	lt1.push_back(Date(2022, 11, 16));
+//
+//	cout << "---------------------------------" << endl;
+//	lt1.emplace_back(2022, 11, 16);
+//
+//	return 0;
+//}
+
+
+
+struct Goods
+{
+	string _name;  // 名字
+	double _price; // 价格
+	int _evaluate; // 评价
+	//...
+
+	Goods(const char* str, double price, int evaluate)
+		:_name(str)
+		, _price(price)
+		, _evaluate(evaluate)
+	{}
+};
+
+//struct ComparePriceLess
+struct Compare1
+{
+	bool operator()(const Goods& gl, const Goods& gr)
+	{
+		return gl._price < gr._price;
+	}
+};
+
+//struct ComparePriceGreater
+struct Compare2
+{
+	bool operator()(const Goods& gl, const Goods& gr)
+	{
+		return gl._price > gr._price;
+	}
+};
+
+//int main()
+//{
+//	vector<Goods> v = { { "苹果", 2.1, 5 }, { "香蕉", 3, 4 }, { "橙子", 2.2, 3 }, { "菠萝", 1.5, 4 } };
+//
+//	/*sort(v.begin(), v.end(), ComparePriceLess());
+//	sort(v.begin(), v.end(), ComparePriceGreater());*/
+//
+//	//sort(v.begin(), v.end(), Compare1());
+//	//sort(v.begin(), v.end(), Compare2());
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {return g1._name < g2._name; });
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {return g1._name > g2._name; });
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {return g1._price < g2._price; });
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {return g1._price > g2._price; });
+//}
+//
+//int main()
+//{
+//	// 两个数相加的lambda
+//	auto add1 = [](int a, int b)->int{return a + b; };
+//	cout << add1(1, 2) << endl;
+//
+//	// 省略返回值
+//	auto add2 = [](int a, int b){return a + b; };
+//	cout << add2(1, 2) << endl;
+//
+//	// 交换变量的lambda
+//	int x = 0, y = 1;
+//	auto swap1 = [](int& x1, int& x2)->void{int tmp = x1; x1 = x2; x2 = tmp; };
+//	swap1(x, y);
+//	cout << x << ":" << y << endl;
+//
+//	// 可以函数形式去写
+//	auto swap2 = [](int& x1, int& x2)
+//	{
+//		int tmp = x1;
+//		x1 = x2;
+//		x2 = tmp;
+//	};
+//
+//	swap2(x, y);
+//	cout << x << ":" << y << endl;
+//
+//	// 不传参数交换x y的lambda  -- 捕捉列表
+//	// 默认捕捉的对象不能修改
+//	/*auto swap3 = [x, y]()mutable   // 传值，但是出了lambda作用域就销毁了
+//	{
+//		int tmp = x;
+//		x = y;
+//		y = tmp;
+//	};
+//
+//	swap3();
+//	cout << x << ":" << y << endl;*/
+//
+//	auto swap3 = [&x, &y]    // 引用，直接改变x和y本身
+//	{
+//		int tmp = x;
+//		x = y;
+//		y = tmp;
+//	};
+//
+//	swap3();
+//	cout << x << ":" << y << endl;
+//
+//
+//	return 0;
+//}
+
+
+
+//int f = 1;
+//int main()
+//{
+//	int a, b, c, d;
+//	a = b = c = d = 1;
+//
+//	// 全部传值捕捉
+//	auto f1 = [=]()->void {
+//		cout << a << b << c << d << endl;
+//	};  // lambda匿名函数的定义
+//	f1(); // lambda匿名函数的调用
+//
+//	// 混合捕捉
+//	auto f2 = [=, &a]()->void {
+//		cout << a << b << c << d << endl;
+//	};
+//	f2();
+//
+//	auto f3 = [&, a, b](){
+//		c++;
+//		d++;
+// 		f++;  // 全局的也可以捕捉
+//		cout << a << b << c << d << endl;
+//	};
+//	f3();
+//	return 0;
+//}
+
+
+class Rate
+{
+public:
+	Rate(double rate) : _rate(rate)
+	{}
+
+	double operator()(double money, int year)
+	{
+		return money * _rate * year;
+	}
+
+private:
+	double _rate;
+};
+
+// lambda_uuid
+class lambda_xxxx
+{
+
+};
+
+//int main()
+//{
+//	// 函数对象
+//	double rate = 0.49;
+//	Rate r1(rate);
+//	r1(10000, 2);
+//
+//	// 仿函数lambda_uuid
+//	// lambda -> lambda_uuid
+//	auto r2 = [=](double monty, int year)->double{return monty*rate*year; };
+//	r2(10000, 2);
+//
+//	auto r3 = [=](double monty, int year)->double{return monty*rate*year; };
+//	r3(10000, 2);
+//
+//	return 0;
+//}
+
+
+template<class F, class T>
+T useF(F f, T x)
+{
+	static int count = 0;
+	cout << "count:" << ++count << endl;
+	cout << "count:" << &count << endl;
+	return f(x);
+}
+
+double f(double i)
+{
+	return i / 2;
+}
+
+struct Functor
+{
+	double operator()(double d)
+	{
+		return d / 3;
+	}
+};
+
 int main()
 {
-	//bit::string str1("hello");
-	//bit::string str2(str1); //  拷贝构造
-	//bit::string str3(move(str1)); // 移动构造
+	// 函数指针
+	cout << useF(f, 11.11) << endl;
 
+	// 函数对象
+	cout << useF(Functor(), 11.11) << endl;
 
-	std::string s1("hello world");
-	std::string s2(s1); // 拷贝构造
+	// lamber表达式对象
+	cout << useF([](double d)->double{ return d / 4; }, 11.11) << endl;
 
-	// std::string s3(s1+s2);
-	std::string s3 = s1 + s2; // 移动构造
-	std::string s4 = move(s1);   // move()之后，s1的资源就可被转移，谨慎使用
 
 	return 0;
 }
