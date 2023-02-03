7088d089e9c1182ee62625b7557c6ace82294218
soukenki <1106671528@qq.com>
2023-02-04 01:23:31 +0900

    2/4 抛异常

--- exception/Test.cpp
+++ exception/Test.cpp
@@ -290,108 +290,181 @@ using namespace std;
 
 
 
-class Exception // 基类
+
+
+
+//class Exception // 基类
+//{
+//public:
+//	Exception(const string& errmsg, int id)
+//		:_errmsg(errmsg)
+//		, _errid(id)
+//	{}
+//
+//	virtual string what() const
+//	{
+//		return _errmsg;
+//	}
+//
+//	int getid() const
+//	{
+//		return _errid;
+//	}
+//
+//protected:
+//	string _errmsg; // 错误信息
+//	int _errid;     // 错误码
+//};
+//
+//class HttpServerException : public Exception  // 派生类 （网络服务）
+//{
+//public:
+//	HttpServerException(const string& errmsg, int id, const string& type)
+//		:Exception(errmsg, id)
+//		, _type(type)
+//	{}
+//	virtual string what() const
+//	{
+//		string str = "HttpServerException:";
+//		str += _type;
+//		str += ":";
+//		str += _errmsg;
+//		return str;
+//	}
+//private:
+//	const string _type;
+//};
+//
+//void SeedMsg(const string& str)
+//{
+//	// 要求出现网络错误重试3次
+//	srand(time(0));
+//	
+//	if (rand() % 3 == 0)
+//	{
+//		throw HttpServerException("网络错误", 100, "get");
+//	}
+//	else if (rand() % 4 == 0)
+//	{
+//		throw HttpServerException("权限不足", 101, "post");
+//	}
+//
+//	cout << "发送成功:"<< str << endl;
+//
+//}
+//void HttpServer()
+//{
+//	// 要求出现网络错误重试3次
+//	string str = "今晚一起看电影怎么样？";
+//	//cin >> str;
+//	int n = 3;
+//	while (n--)
+//	{
+//		try
+//		{
+//			SeedMsg(str);
+//
+//			break;   // 没有异常，跳出循环
+//		}
+//		catch (const Exception& e)
+//		{
+//			// 网络错误 且 重试3次
+//			if (e.getid() == 100 && n > 0)
+//			{
+//				continue; // 重试
+//			}
+//			else
+//			{
+//				throw e; // 重新抛出
+//			}		
+//		}
+//	}
+//
+//
+//}
+//int main()
+//{
+//	while (1)
+//	{
+//		//this_thread::sleep_for(chrono::seconds(1));
+//		Sleep(1000);
+//
+//		try
+//		{
+//			HttpServer();
+//		}
+//		catch (const Exception& e) // 这里捕获父类对象就可以
+//		{
+//			// 多态
+//			cout << e.what() << endl;
+//			// 抛出来的what是进行过重写过的，重写到哪个时抛异常，就会读到那一个
+//
+//			// ...记录日志
+//		}
+//		catch (...)
+//		{
+//			cout << "Unkown Exception" << endl;
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+double Division(int a, int b)
 {
-public:
-	Exception(const string& errmsg, int id)
-		:_errmsg(errmsg)
-		, _errid(id)
-	{}
-
-	virtual string what() const
+	// 当b == 0时抛出异常
+	if (b == 0)
 	{
-		return _errmsg;
+		throw "Division by zero condition!";
 	}
+	return (double)a / (double)b;
+}
 
-	int getid() const
-	{
-		return _errid;
-	}
-
-protected:
-	string _errmsg; // 错误信息
-	int _errid;     // 错误码
-};
-
-class HttpServerException : public Exception  // 派生类 （网络服务）
+void Func()
 {
-public:
-	HttpServerException(const string& errmsg, int id, const string& type)
-		:Exception(errmsg, id)
-		, _type(type)
-	{}
-	virtual string what() const
-	{
-		string str = "HttpServerException:";
-		str += _type;
-		str += ":";
-		str += _errmsg;
-		return str;
-	}
-private:
-	const string _type;
-};
-
-void SeedMsg(const string& str)
-{
-	// 要求出现网络错误重试3次
-	srand(time(0));
+	// 这里可以看到如果发生除0错误抛出异常，另外下面的array没有得到释放。
+	// 所以这里捕获异常后并不处理异常，异常还是交给外面处理，这里捕获了再
+	// 重新抛出去。
 	
-	if (rand() % 3 == 0)
+	// 隐患，第一个new成功，第二个new失败抛异常，导致第一个创建的内存泄露
+	// 可以解决，但是很麻烦，这样的问题一般是用智能指针解决
+	int* array1 = new int[10];
+	int* array2 = new int[10];  //隐患
+	try 
 	{
-		throw HttpServerException("请求资源不存在", 100, "get");
+		// int* array2 = new int[10];  //写在这里勉强解决隐患，但是可读性差
+		int len, time;
+		cin >> len >> time;
+		cout << Division(len, time) << endl;
 	}
-	else if (rand() % 4 == 0)
+	catch (...)    // 这里必须接收，为了释放内存
 	{
-		throw HttpServerException("权限不足", 101, "post");
+		cout << "delete []" << array1 << endl;
+		delete[] array1;
+
+		cout << "delete []" << array2 << endl;
+		delete[] array2;
+
+		throw;  // 捕获什么就抛出什么
 	}
+	// ...
+	cout << "delete []" << array1 << endl;
+	delete[] array1;
 
-	cout << "发送成功:"<< str << endl;
-
+	cout << "delete []" << array2 << endl;
+	delete[] array2;
 }
-void HttpServer()
-{
-	// 要求出现网络错误重试3次
-	string str;
-	cin >> str;
-	int n = 3;
-	try
-	{
-		SeedMsg(str);
-	}
-	catch (const Exception& e)
-	{
-		if (e.getid() == 100 && n)
-		{
-			SeedMsg(str);
-			n--;
-		}
 
-		throw e; // 重新抛出
-	}
-}
 int main()
 {
-	while (1)
+	try
 	{
-		//this_thread::sleep_for(chrono::seconds(1));
-		Sleep(1000);
-
-		try
-		{
-			HttpServer();
-		}
-		catch (const Exception& e) // 这里捕获父类对象就可以
-		{
-			// 多态
-			cout << e.what() << endl;
-			// 抛出来的what是进行过重写过的，重写到哪个时抛异常，就会读到那一个
-		}
-		catch (...)
-		{
-			cout << "Unkown Exception" << endl;
-		}
+		Func();
+	}
+	catch (const char* errmsg)
+	{
+		cout << errmsg << endl;
 	}
 	return 0;
 }
-
